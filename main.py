640c4cc035d82b8307d522237a322eb9891f4ebb
Luigi Maglanoc <luigi.maglanoc@usit.uio.no>
2024-04-08 09:54:58 +0200

    Include effort in battle logic and some required refactoring

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import httpx
 from logging_config import (
     get_logger,
 )  # Adjust the import statement according to your project structure
-from typing import List
+from typing import List, Dict
 import random
 import csv
 
@@ -22,13 +22,21 @@ templates = Jinja2Templates(directory="templates")
 class Pokemon(BaseModel):
     name: str
     level: int = 1
-    hp: int
-    attack: int
-    defense: int
-    special_attack: int
-    special_defense: int
-    speed: int
+
+    # TODO: later find out if can use type annotation
+    # to get attribute with dict{base_stat: int, effort: int}
+
+    hp: Dict[str, int]
+    attack: Dict[str, int]
+    defense: Dict[str, int]
+    special_attack: Dict[str, int]
+    special_defense: Dict[str, int]
+    speed: Dict[str, int]
     types: List[str]
+
+    # TODO: consider having new attributes, that
+    # will have stored after apply_stat_modifier
+    # e.g. hp_actual, defense_actual, etc.
 
     # Removed IV as (class) attribute, as
     # 1) there is one IV for each stat and also
@@ -37,20 +45,59 @@ class Pokemon(BaseModel):
     class Config:
         pass
 
-    def stat_modifier(self, stat, IV):
-        # TODO: final should be something like
-        # Stat = ((2 x BaseStat + IV + (EV/4)) x Level / 100)
-        return int((2 * stat + IV) * self.level / 100)
+    # TODO: final should be something like
+    # Stat = ((2 x BaseStat + IV + (EV/4)) x Level / 100)
 
-    def apply_stat_modifier(
-        self,
-        stats={"attack", "special_attack", "speed", "hp", "defense", "special_defense"},
-    ):
-        for stat in stats:
-            # IV for each stat. It is random so not stored as class attribute
-            IV = random.randint(0, 31)
-            if hasattr(self, stat):
-                setattr(self, stat, self.stat_modifier(getattr(self, stat), IV))
+    # TODO: later figure out how to do in loop (previous implementation
+    # was unnecessarily complicated)
+
+    # NOTE: each `random.randint(0, 31)` represents IV.
+    # later figure out if make into function, or class attribute
+    def stats_modifier(self):
+        self.hp["base_stat"] = int(
+            (2 * self.hp["base_stat"] + random.randint(0, 31) + (self.hp["effort"] / 4))
+            * (self.level / 100)
+        )
+        self.attack["base_stat"] = int(
+            (
+                2 * self.attack["base_stat"]
+                + random.randint(0, 31)
+                + (self.attack["effort"] / 4)
+            )
+            * (self.level / 100)
+        )
+        self.defense["base_stat"] = int(
+            (
+                2 * self.defense["base_stat"]
+                + random.randint(0, 31)
+                + (self.defense["effort"] / 4)
+            )
+            * (self.level / 100)
+        )
+        self.special_attack["base_stat"] = int(
+            (
+                2 * self.special_attack["base_stat"]
+                + random.randint(0, 31)
+                + (self.special_attack["effort"] / 4)
+            )
+            * (self.level / 100)
+        )
+        self.special_defense["base_stat"] = int(
+            (
+                2 * self.special_defense["base_stat"]
+                + random.randint(0, 31)
+                + (self.special_defense["effort"] / 4)
+            )
+            * (self.level / 100)
+        )
+        self.speed["base_stat"] = int(
+            (
+                2 * self.speed["base_stat"]
+                + random.randint(0, 31)
+                + (self.speed["effort"] / 4)
+            )
+            * (self.level / 100)
+        )
 
 
 # Initialize list with all pokemon names
@@ -85,7 +132,9 @@ async def parse_types_csv():
         csv_reader = csv.reader(file)
         headers = next(csv_reader)
         for row in csv_reader:
-            row_dict = {header: value for header, value in zip(headers[1:], row[1:])}
+            row_dict = {
+                header: float(value) for header, value in zip(headers[1:], row[1:])
+            }
             type_advantages[row[0]] = row_dict
 
 
@@ -107,12 +156,16 @@ async def get_pokemon(pokemon_name: str):
         response = await client.get(url)
         if response.status_code == 200:
             pokemon_data = response.json()
+
             stats = {
-                stat["stat"]["name"]: stat["base_stat"]
+                stat["stat"]["name"]: {
+                    "base_stat": stat["base_stat"],
+                    "effort": stat["effort"],
+                }
                 for stat in pokemon_data["stats"]
             }
-            stats = clean_stat_names(stats)
 
+            stats = clean_stat_names(stats)
             types = [t["type"]["name"] for t in pokemon_data["types"]]
             pokemon = Pokemon(name=pokemon_name, **stats, types=types)
             sprites = pokemon_data["sprites"]
@@ -126,41 +179,37 @@ async def get_pokemon(pokemon_name: str):
 
 def battle_simulator(pokemon1: Pokemon, pokemon2: Pokemon, type_advantages: dict):
 
-    pokemon1.apply_stat_modifier()
-    pokemon2.apply_stat_modifier()
+    pokemon1.stats_modifier()
+    pokemon2.stats_modifier()
 
-    if pokemon1.speed > pokemon2.speed:
-        attacker = pokemon1
-        defender = pokemon2
-    else:
-        attacker = pokemon2
-        defender = pokemon1
+    while pokemon1.hp["base_stat"] > 0 and pokemon2.hp["base_stat"] > 0:
+        attacker, defender = (
+            (pokemon1, pokemon2)
+            if pokemon1.speed["base_stat"] > pokemon2.speed["base_stat"]
+            else (pokemon2, pokemon1)
+        )
 
-    while attacker.hp > 0 and defender.hp > 0:
-        # Loop through each type of the attacker
         for atk_type in attacker.types:
-            attack_power = (attacker.attack + attacker.special_attack) / 2
-            defense_power = (defender.defense + defender.special_defense) / 2
+            attack_power = (
+                attacker.attack["base_stat"] + attacker.special_attack["base_stat"]
+            ) / 2
+            defense_power = (
+                defender.defense["base_stat"] + defender.special_defense["base_stat"]
+            ) / 2
             damage = max(1, int((attack_power - defense_power) / 2))
 
             type_effectiveness = 1
-            # Consider each type of the defender
             for defending_type in defender.types:
                 type_effectiveness *= type_advantages.get(atk_type, {}).get(
                     defending_type, 1
                 )
 
-            # Apply cumulative type effectiveness
             damage *= type_effectiveness
-            defender.hp -= damage
+            defender.hp["base_stat"] -= damage
 
-        # Players switch roles for the next round
-        attacker, defender = (
-            defender,
-            attacker,
-        )
+        pokemon1, pokemon2 = defender, attacker
 
-    return pokemon1.name if pokemon2.hp <= 0 else pokemon2.name
+    return attacker.name if pokemon2.hp["base_stat"] <= 0 else defender.name
 
 
 # Custom HTTPException handler for FastAPI
@@ -199,6 +248,7 @@ async def read_pokemon(request: Request, pokemon_name: str):
         del pokemon_stats["name"]
         pokemon_types = pokemon.types
         del pokemon_stats["types"]
+        del pokemon_stats["level"]
         pokemon_stats = revert_stat_names(pokemon_stats)
 
         response = templates.TemplateResponse(
